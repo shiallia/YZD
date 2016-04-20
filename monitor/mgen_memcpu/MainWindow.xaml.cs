6ac3c91f491986199918db40ae9d52eee731fa9c
zhangfangithub <magiczhang@outlook.com>
2016-04-20 16:17:01 +0800

    增加了监控程序的代码

---
+++ monitor/mgen_memcpu/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Linq;
+//using System.Management;
+//using System.Net;
+//using System.Net.NetworkInformation;
+//using System.Text;
+using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+//using System.Windows.Documents;
+//using System.Windows.Input;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Navigation;
+//using System.Windows.Shapes;
+
+namespace mgen_memcpu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ulong totMem;
+        Utility util;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            util = new Utility();
+            totMem = util.GetPhysicalMemSize();
+            System.Threading.ThreadPool.QueueUserWorkItem(WorkerItem, new Action<float, ulong, string, string>(Callback));
+            memTotal.Text = ConvertBytes(totMem, 3) + " GB";
+        }
+
+        void WorkerItem(object arg)
+        {
+            var callback = (Action<float, ulong, string, string>)arg;
+            util.Monitor(callback);
+        }
+
+        void Callback(float cpu, ulong mem , string ip , string yzdmem)
+        {
+            var pcpu = Math.Round(cpu, 2, MidpointRounding.AwayFromZero);
+            var used_mem=totMem-mem;
+            var mb_mem = ConvertBytes(used_mem, 2);
+            var pmem = ConvertBytes(Convert.ToUInt64(mb_mem), 1);
+
+            Dispatcher.Invoke((Action)(() =>
+                {
+                    cpuT.Text = pcpu + "%";
+                    cpuP.Value = pcpu;
+                    memT.Text = String.Format("内存总占用：{0} GB ({1:N} MB)", pmem, mb_mem);
+                    yzdmemT.Text = "医诊达程序占用："+yzdmem+"MB"; 
+                    memP.Value = (double)(used_mem / Convert.ToDecimal(totMem) * 100);
+                    ipT.Text = ip;
+                }));
+        }
+
+        decimal ConvertBytes(ulong b, int iteration)
+        {
+            long iter = 1;
+            for (int i = 0; i < iteration; i++)
+                iter *= 1024;
+            return Math.Round((Convert.ToDecimal(b)) / Convert.ToDecimal(iter), 2, MidpointRounding.AwayFromZero);
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            util.Dispose();
+        }
+    }
+}
---
+++ monitor/mgen_memcpu/Utility.cs
@@ -0,0 +1,77 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+using System.Diagnostics;
+//using System.Management;
+using Microsoft.VisualBasic.Devices;
+using System.Net.NetworkInformation;
+
+namespace mgen_memcpu
+{
+    class Utility : IDisposable
+    {
+        public ulong GetPhysicalMemSize()
+        {
+            return cinf.TotalPhysicalMemory;
+        }
+
+        PerformanceCounter cpu;
+        ComputerInfo cinf;
+
+        public Utility()
+        {
+            cpu = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            cinf = new ComputerInfo();
+            
+        }
+
+        public void Monitor(Action<float, ulong ,string ,string> callback)
+        {
+            while (true)
+            {
+                //cpu占用
+                var percentage = cpu.NextValue();
+                //内存占用
+                var mem = cinf.AvailablePhysicalMemory;
+
+                //当前ip
+                string localIP = string.Empty;
+                NetworkInterface[] interfaces = System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces();
+                int len = interfaces.Length;
+                for (int i = 0; i < len; i++)
+                {
+                    NetworkInterface ni = interfaces[i];
+                    if (ni.Name == "本地连接")
+                    {
+                        IPInterfaceProperties property = ni.GetIPProperties();
+                        foreach (UnicastIPAddressInformation ip in property.UnicastAddresses)
+                        {
+                            if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                            {
+                                localIP = ip.Address.ToString();                                
+                            }
+                        }
+                    }
+                }
+
+                //医诊达进程内存占用
+                Process[] myPro = Process.GetProcessesByName("YzdSmartBox");
+                string yzdMem = string.Empty;
+                if(myPro.Length != 0)
+                {
+                    yzdMem = (myPro[0].PrivateMemorySize64 / 1024 / 1024).ToString();
+                }
+                
+                callback(percentage, mem , localIP , yzdMem);
+
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+        public void Dispose()
+        {
+            cpu.Dispose();
+        }
+    }
+}
